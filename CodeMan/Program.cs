2adc368364b22292b0f3eb987d0e71106fa56a39
thecryptoalien@gmail.com <thecryptoalien@gmail.com>
2022-07-28 01:08:34 -0400

    Add project files.

---
+++ CodeMan/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeMan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // im crazy but maybe I will start with python
+
+            // why not c++ and c
+
+            // got c++ done why not java with c++ wrapper lmao
+
+            // what's next fortran or perl
+
+            // why not try cobol
+
+            // All OF THE LANGUAGES LMAO
+        }
+    }
+}
